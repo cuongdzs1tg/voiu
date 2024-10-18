47cf6c9529078eec0e265f7895b867a4838ff03a
cuongdzs1tg <hahacuong2004@gmail.com>
2024-10-18 22:05:09 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Vo iu Cua Anh:33';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100049923911633';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng vo iuu';
+const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
